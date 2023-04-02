b62c1b0d2806c5f5e154dfe46dc62f2264e225d9
Hwang yena <center7332@naver.com>
2023-04-02 21:34:52 +0900

    feat: add routing

---
+++ src/pages/cake/index.tsx
@@ -0,0 +1,7 @@
+export default function CakePage() {
+  return (
+    <div>
+      <h1>생일 케이크 링크 제목</h1>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,10 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main>rolling cake</main>;
+  return (
+    <main>
+      rolling cake
+      <button>카카오 로그인</button>
+    </main>
+  );
 }
