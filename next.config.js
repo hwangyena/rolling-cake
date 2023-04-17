8ad191d569be16c6e0bcf561943add8ce7fc1f61
Hwang yena <center7332@naver.com>
2023-04-17 22:28:34 +0900

    feat: use Next13 - app directory

--- next.config.js
+++ next.config.js
@@ -5,11 +5,18 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  experimental: {
+    appDir: true,
+  },
   async rewrites() {
     return [
       {
         source: '/oauth/:path*',
         destination: `${process.env.KAKAO_URL}/oauth/:path*`,
+      },
+      {
+        source: '/v2/:path*',
+        destination: `${process.env.KAKAO_URL}/v2/:path*`,
       },
     ];
   },
