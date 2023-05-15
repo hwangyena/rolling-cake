a071c57900f78d893bcbc57669094cf2899268b2
Hwang yena <center7332@naver.com>
2023-05-16 01:09:30 +0900

    feat: remove next-auth & get kakao-code

--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import { signIn } from 'next-auth/react';
 import Image from 'next/image';
 
 const LoginButton = () => {
-  // const { data: session } = useSession(); //FIXME: use it next!
-
   return (
-    <button
-      className="w-full h-[70px] rounded-lg relative bg-slate-300"
-      onClick={() => signIn('kakao')}>
-      <Image src={'/images/kakao_login_large_wide.png'} alt="kakao_login" fill />
-    </button>
+    <a
+      href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT}&response_type=code`}>
+      <button className="w-full h-[70px] rounded-lg relative bg-slate-300">
+        <Image src={'/images/kakao_login_large_wide.png'} alt="kakao_login" fill />
+      </button>
+    </a>
   );
 };
 
