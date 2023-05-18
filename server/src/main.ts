598640c91c1d5dc805182f9a01f044a2381e5844
Hwang yena <center7332@naver.com>
2023-05-18 23:43:58 +0900

    feat: server - get kakao token

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 import { Application } from 'https://deno.land/x/oak/mod.ts';
 
 import router from './routes.ts';
 
 const app = new Application();
 
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
---
+++ server/src/typings/auth.d.ts
@@ -0,0 +1,9 @@
+type Kakao_Token = {
+  access_token: string;
+  token_type: 'bearer';
+  refresh_token: string;
+  id_token: string;
+  expires_in: number;
+  scope: string;
+  refresh_token_expires_in: number;
+};
