6187ed4a87e434a1d2f11f9cb887b4d8adcfec44
Hwang yena <center7332@naver.com>
2023-05-24 21:54:58 +0900

    feat: add User DB - create, read

---
+++ server/src/db/user.ts
@@ -0,0 +1,23 @@
+import DB from "./index.ts";
+
+class User {
+  async selectAll() {
+    return await DB.from('User').select()
+  }
+
+  async selectById(id: string) {
+    return await DB.from('User').select().eq('id',id)
+  }
+
+  async create(id: string, name: string) {
+    return await DB.from('User').insert({ id, name });
+  }
+}
+
+// const { data: updataeData, error: updateError } = await DB
+//   .from('User')
+//   .update({ name: 'Yena' })
+//   .eq('id', 1);
+
+
+export default new User();
