25f311a675bec0cd30a271bc14c366a2218bbed8
Hwang yena <center7332@naver.com>
2023-06-06 16:54:05 +0900

    feat: get user api

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,8 +1,11 @@
 import { Router } from '../deps.ts';
 import login from './auth.ts';
+import { getUser } from './user.ts';
 
 const router = new Router();
 
 router.post('/login', login);
+
+router.get('/user/:id', getUser);
 
 export default router;
