37ecb23d1610b93627a7ac7542279629ed583b18
Hwang yena <center7332@naver.com>
2023-07-08 00:02:34 +0900

    feat: add design system by tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,50 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          light: '#b5ffd7',
+          primary: '#82ffce',
+          dark: '#0fc89b',
+        },
+        pink: {
+          light: '#ff8d97',
+          primary: '#ff78f1',
+        },
+        gray: {
+          100: '#f5f5f5',
+          200: '#f0f0f0',
+          300: '#e0e0e0',
+          400: '#dddddd',
+          500: '#bbbbbb',
+          600: '#888888',
+          700: '#444444',
+          800: '#333333',
+          900: '#111111',
+        },
+      },
+      fontFamily: {
+        apple: ['-apple-system', 'sans-serif'],
+        neo: ['neodgm'],
+      },
+      fontSize: {
+        h1: ['24px', { lineHeight: '140%' }],
+        h2: ['22px', { lineHeight: '140%' }],
+        h3: ['20px', { lineHeight: '150%' }],
+        h4: ['18px', { lineHeight: '150%' }],
+        h5: ['24px', { lineHeight: '140%' }],
+        t1: ['16px', { lineHeight: '140%' }],
+        t2: ['16px', { lineHeight: '150%' }],
+        b1: ['16px', { lineHeight: '140%' }],
+        b2: ['14px', { lineHeight: '140%' }],
+        b3: ['12px', { lineHeight: '140%' }],
+        btn: ['16px', { lineHeight: '120%' }],
+        cap: ['10px', { lineHeight: '120%' }],
+        effect_t: ['22px', { lineHeight: '140%' }],
+        effect_b: ['14px', { lineHeight: '140%' }],
+      },
+    },
   },
   plugins: [],
 };
