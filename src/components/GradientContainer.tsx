e636961428f107bc30674793aa6feae2b55be621
SK플래닛 황예나 <hwangyena@sk.com>
2023-10-06 13:42:50 +0900

    style: navigation style

--- src/components/GradientContainer.tsx
+++ src/components/GradientContainer.tsx
@@ -4,7 +4,7 @@ const GradientContainer = ({
   type,
   children,
 }: PropsWithChildren<{
-  type: 'green-circle' | 'grid';
+  type: 'green-circle' | 'grid' | 'grid-with-gradient';
 }>) => {
   if (type === 'green-circle') {
     return <div className="fixed w-full h-full green-circle-gradient">{children}</div>;
@@ -12,6 +12,14 @@ const GradientContainer = ({
 
   if (type === 'grid') {
     return <div className="bg-grid-pattern w-full h-full bg-contain">{children}</div>;
+  }
+
+  if (type === 'grid-with-gradient') {
+    return (
+      <div className="green-pink-gradient-with-grid w-full h-full bg-contain flex flex-col">
+        {children}
+      </div>
+    );
   }
 
   return <div>{children}</div>;
