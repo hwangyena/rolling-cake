fffec09ef090df13f1b98e5b51e734579063effb
Hwang yena <center7332@naver.com>
2023-10-07 23:46:17 +0900

    feat: show popup if not owner of letter

--- src/components/common/Popup.tsx
+++ src/components/common/Popup.tsx
@@ -9,7 +9,7 @@ import Button from './Button';
 const Popup = () => {
   const [store, setStore] = useAtom(popupStore);
 
-  const { content, title, onCancel, onConfirm } = store as Popup;
+  const { content, title, onCancel, onConfirm, hideCancel, bottomNode } = store as Popup;
 
   const onCancelClicked = useCallback(() => {
     setStore(null);
@@ -28,21 +28,28 @@ const Popup = () => {
   return (
     <div className={styles.popup}>
       <div className={styles['popup-content']}>
-        <h4 className="text-h4 text-gray-800 font-bold">{title}</h4>
-        <p
-          className="text-b1 pt-[20px] text-gray-800"
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-        <div className={styles['button-wrapper']}>
-          <Button type="SMALL" color="gray" onClick={onCancelClicked}>
-            취소
-          </Button>
+        <section className="mb-8">
+          <h4 className="text-h4 text-gray-800 font-bold whitespace-pre">{title}</h4>
+          {content && (
+            <p
+              className="text-b1 pt-[20px] text-gray-800"
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
+          )}
+        </section>
+        <section className={hideCancel ? 'flex justify-center' : 'flex justify-evenly'}>
+          {!hideCancel && (
+            <Button type="SMALL" color="gray" onClick={onCancelClicked}>
+              취소
+            </Button>
+          )}
           <Button type="SMALL" color="green" onClick={onConfirmClicked}>
             확인
           </Button>
-        </div>
+        </section>
+        {bottomNode}
       </div>
-      <div className="absolute left-0 top-0 bottom-0 right-0 bg-black opacity-[0.7] z-0" />
+      <div className="absolute left-0 top-0 bottom-0 right-0 bg-black opacity-[0.7] z-50" />
     </div>
   );
 };
