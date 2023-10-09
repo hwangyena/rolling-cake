69fe5e179615c0817ac8e39576d2a6c466487bce
Hwang yena <center7332@naver.com>
2023-10-09 22:17:57 +0900

    refactor: get api url by util

--- src/app/cake/[id]/page.tsx
+++ src/app/cake/[id]/page.tsx
@@ -3,19 +3,16 @@ import getUser from '@/actions/getUser';
 import ClientOnly from '@/components/ClientOnly';
 import EmptyCakeClient from './EmptyCakeClient';
 import HaveCakeClient from './HaveCakeClient';
+import { Cake } from '@prisma/client';
+// import { getBaseUrl } from '@/lib/utils';
 
 export default async function CakePage({ params }: { params: { id: string } }) {
-  const cakes = await fetch(
-    `${
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000/api'
-        : 'https://rolling-cake.vercel.app/api'
-    }/cake/${params.id}`,
-    {
-      method: 'GET',
-      cache: 'no-store',
-    }
-  ).then((res) => res.json());
+  // const cakes = await fetch(`${getBaseUrl()}/cake/${params.id}`, {
+  //   method: 'GET',
+  //   cache: 'no-store',
+  // }).then((res) => res.json());
+
+  const cakes = [] as Cake[];
 
   const user = await getUser(params.id);
   const loginUser = await getCurrentUser();
