920e052b69cc9574e717c52798f906edf1cae7f4
Hwang yena <center7332@naver.com>
2023-10-09 22:27:35 +0900

    feat: express server

--- src/app/cake/[id]/page.tsx
+++ src/app/cake/[id]/page.tsx
@@ -3,16 +3,13 @@ import getUser from '@/actions/getUser';
 import ClientOnly from '@/components/ClientOnly';
 import EmptyCakeClient from './EmptyCakeClient';
 import HaveCakeClient from './HaveCakeClient';
-import { Cake } from '@prisma/client';
-// import { getBaseUrl } from '@/lib/utils';
+import { getBaseUrl } from '@/lib/utils';
 
 export default async function CakePage({ params }: { params: { id: string } }) {
-  // const cakes = await fetch(`${getBaseUrl()}/cake/${params.id}`, {
-  //   method: 'GET',
-  //   cache: 'no-store',
-  // }).then((res) => res.json());
-
-  const cakes = [] as Cake[];
+  const cakes = await fetch(`${getBaseUrl()}/cake/${params.id}`, {
+    method: 'GET',
+    cache: 'no-store',
+  }).then((res) => res.json());
 
   const user = await getUser(params.id);
   const loginUser = await getCurrentUser();
