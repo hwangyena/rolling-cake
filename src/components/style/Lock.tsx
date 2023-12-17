5e0e94094a69a9a64f1a6fc33879e7fe0361fdcf
Hwang yena <center7332@naver.com>
2023-12-17 22:58:14 +0900

    style: lock size

--- src/components/style/Lock.tsx
+++ src/components/style/Lock.tsx
@@ -6,13 +6,13 @@ const Lock = ({ unlock = false, onClick }: { unlock?: boolean; onClick?: () => v
     <div className="relative mt-1 flex flex-col items-center justify-center" onClick={onClick}>
       <div
         className={cn(
-          'absolute bottom-[90%] h-[7px] w-[9px] rounded-r-[10px] rounded-t-[50px] border-[5px] border-b-0 border-l-[2px] border-r-[2px] border-t-[2px] border-gray-600 transition-all	',
-          { 'origin-left rotate-[-20deg] border-green-dark': unlock },
+          'absolute bottom-[90%] h-[10px] w-[12px] rounded-r-[10px] rounded-t-[50px] border-[5px] border-b-0 border-l-[2px] border-r-[2px] border-t-[2px] border-gray-600 transition-all	',
+          { 'origin-left rotate-[-30deg] border-green-dark': unlock },
         )}
       />
       <div
         className={cn(
-          'z-10 flex h-[12px] w-[12px] items-center justify-center rounded-[3px] bg-gray-600 transition-all	',
+          'z-10 flex h-[15px] w-[15px] items-center justify-center rounded-[3px] bg-gray-600 transition-all	',
           { 'bg-green-dark': unlock },
         )}
       />
