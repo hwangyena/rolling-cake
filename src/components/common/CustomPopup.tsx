9f334f5c82dae070a714143063e9d627bf7310ad
Hwang yena <center7332@naver.com>
2025-05-24 22:57:14 +0900

    style: 스타일 수정

--- src/components/common/CustomPopup.tsx
+++ src/components/common/CustomPopup.tsx
@@ -2,16 +2,15 @@ import Image from 'next/image';
 import { PropsWithChildren, memo, useEffect } from 'react';
 
 import ShadowCard from '../style/ShadowCard';
+import { SmallButton } from './Button';
 
 const CustomPopup = ({ content, title, hasIcon, onConfirm }: CustomPopup) => {
   // open terms page
   useEffect(() => {
     const termsElement = document.querySelector('#terms') as HTMLElement;
-
     if (!termsElement) {
       return;
     }
-
     const handleTermsClicked = () => {
       window.open('https://yesmesecret.notion.site/e8db1bedb4074a2b89bd754dcd440c07?pvs=4');
     };
@@ -22,26 +21,28 @@ const CustomPopup = ({ content, title, hasIcon, onConfirm }: CustomPopup) => {
 
   return (
     <Wrapper type={title}>
-      <article className="items-top flex h-[24px] w-full justify-between rounded-t-lg bg-pink-200 px-[6px] pt-[2px] font-neo text-effect_b">
-        <span className="text-white">{title}</span>
+      <div className="items-top flex w-full justify-between rounded-t-lg bg-secondary-pink-50 px-[7px] py-[6px]">
+        <span className=" font-neo text-effect_b text-white">{title}</span>
         <button
-          className={`black-shadow relative flex aspect-square h-[80%] items-center justify-center rounded-md bg-white before:h-[60%] before:w-[2px] before:rotate-90 before:bg-black before:content-[""]`}
+          className={`black-shadow relative flex w-[18px] h-[18px] items-center justify-center rounded-md bg-white before:h-[60%] before:w-[2px] before:rotate-90 before:bg-black before:content-[""]`}
           onClick={onConfirm}
         />
-      </article>
-      <article className="flex gap-3 px-3 py-5 text-b2">
-        {hasIcon && (
-          <div className="relative aspect-square w-[25%]">
-            <Image src={'/images/custom-alert.png'} alt="icon" fill></Image>
-          </div>
-        )}
-        <p className="flex-1" dangerouslySetInnerHTML={{ __html: content ?? '' }} />
-      </article>
-      <article className="flex w-full justify-center pb-3">
-        <button className="black-shadow rounded-2xl px-[24px] py-[5px] text-b3" onClick={onConfirm}>
+      </div>
+      <div className="px-[10px] py-[20px]">
+        <div className="flex gap-3 text-b2 items-center justify-center">
+          {hasIcon && (
+            <div className="relative aspect-square w-[25%]">
+              <Image src={'/images/custom-alert.png'} alt="icon" fill></Image>
+            </div>
+          )}
+          <p dangerouslySetInnerHTML={{ __html: content ?? '' }} />
+        </div>
+        <button
+          className="black-shadow rounded-2xl px-[22px] py-[7px] text-btn2 font-bold mt-[10px]"
+          onClick={onConfirm}>
           확인
         </button>
-      </article>
+      </div>
       <div className="dimmed" />
     </Wrapper>
   );
@@ -58,7 +59,7 @@ const Wrapper = ({ children, type }: PropsWithChildren<{ type: CustomPopup['titl
 
   if (type === 'Welcome!') {
     return (
-      <div className="absolute bottom-3 flex w-full items-end justify-center px-3 pb-7">
+      <div className="absolute bottom-3 flex w-full items-end justify-center px-[30px] pb-[50px]">
         <ShadowCard className="h-fit w-full rounded-lg p-[4px] text-center">{children}</ShadowCard>
       </div>
     );
