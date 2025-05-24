ee0522f7bbfdb395f9311bd8a49ec05ed47a0da8
yesme <70925962+hwangyena@users.noreply.github.com>
2025-05-24 23:28:39 +0900

    style: 케이크 상세 페이지 스타일 수정 (#188)

--- src/app/(pages)/cake/[id]/_components/ShareButton.tsx
+++ src/app/(pages)/cake/[id]/_components/ShareButton.tsx
@@ -1,35 +1,18 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-
 import { BigButton } from '@components/common/Button';
 
 import { useSnackbar } from '@lib/provider/SnackbarProvider';
 
-type Props = {
-  userId: string;
-  isOwn?: boolean;
-};
-
-const ShareButton = ({ userId, isOwn }: Props) => {
-  const router = useRouter();
+const ShareButton = () => {
   const snackbar = useSnackbar();
 
   const handleClicked = () => {
-    if (isOwn) {
-      navigator.clipboard.writeText(window.location.href);
-      snackbar.show('링크를 복사했어요! SNS에 붙여넣어 공유해봐요');
-      return;
-    } else {
-      router.push(`/make/${userId}?step=sheet`);
-    }
+    navigator.clipboard.writeText(window.location.href);
+    snackbar.show('링크를 복사했어요! SNS에 붙여넣어 공유해봐요');
   };
 
-  return (
-    <BigButton onClick={handleClicked}>
-      {isOwn ? '링크 공유하고 케이크 받기' : '롤링케이크 만들어주기'}
-    </BigButton>
-  );
+  return <BigButton onClick={handleClicked}>링크 공유하고 케이크 받기</BigButton>;
 };
 
 export default ShareButton;
--- src/app/(pages)/cake/[id]/_components/index.ts
+++ src/app/(pages)/cake/[id]/_components/index.ts
@@ -1,4 +1,5 @@
 export { default as ShareButton } from './ShareButton';
+export { default as MakeButton } from './MakeButton';
 export { default as EmptyCake } from './EmptyCake';
 export { default as HaveCake } from './HaveCake';
 export { default as Cake } from './Cake';
