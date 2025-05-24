d015fae777a821fefca38bd08a88d3dfedf20b9c
Hwang yena <center7332@naver.com>
2025-05-24 22:41:22 +0900

    feat: welcome - metadata 수정

--- src/app/(pages)/welcome/page.tsx
+++ src/app/(pages)/welcome/page.tsx
@@ -1,6 +1,7 @@
 import GradientContainer from '@/components/GradientContainer';
 import Cake from '@/components/cake/Cake';
-import { Metadata } from 'next';
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
 import 'server-only';
 
@@ -8,21 +9,37 @@ import { getCurrentUser } from '@service/user';
 
 import { WelcomePopup } from './_components';
 
-export const metadata: Metadata = {
-  title: 'Welcome!',
-  description: '환영해요! 친구들에게 축하 받을 수 있도록 롤링케이크 이름을 지어볼까요?',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const user = await getCurrentUser();
+  try {
+    if (!user) {
+      return {
+        title: '404 - 롤링케이크',
+      };
+    }
+
+    return {
+      title: 'Welcome! - 롤링케이크',
+      description: '환영해요! 친구들에게 축하 받을 수 있도록 롤링케이크 이름을 지어볼까요?',
+    };
+  } catch (err) {
+    return {
+      title: '404 - 롤링케이크',
+    };
+  }
+}
 
 export default async function Welcome() {
   const user = await getCurrentUser();
-
+  if (!user) {
+    notFound();
+  }
   return (
     <GradientContainer type="blueCircle" className="flex flex-col items-center justify-between">
       <div className="grid h-full w-full flex-1 place-items-center p-[10%]">
         <Cake className="aspect-square w-[100%]" />
       </div>
-
-      <WelcomePopup user={user!} />
+      <WelcomePopup user={user} />
     </GradientContainer>
   );
 }
