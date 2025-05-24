f2e5e4d1c5d6d08a520f541e8feb1c76f00a8c5a
Hwang yena <center7332@naver.com>
2025-05-24 23:34:02 +0900

    feat: make - 초기에는 prevbutton 제거

--- src/app/(pages)/make/[userId]/_components/NextStep.tsx
+++ src/app/(pages)/make/[userId]/_components/NextStep.tsx
@@ -2,7 +2,7 @@
 
 import { useBlock, useStep } from '@/lib/hooks/make';
 import { useParams, useRouter } from 'next/navigation';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { useStepValidation } from '@app/(pages)/make/[userId]/_provider';
 
@@ -15,6 +15,7 @@ const NextStep = () => {
   const { onBackClicked: handleBackClicked } = useBlock();
   const { step, stepData } = useStep();
 
+  const hidePrevButton = useMemo(() => step === 'sheet', [step]);
   const isNextDisabled = !validation.isValid && step === 'letter';
   const handleNextClicked = useCallback(() => {
     if (stepData?.next === 'complete') {
@@ -25,8 +26,9 @@ const NextStep = () => {
     router.push(`/make/${params?.userId}?step=${stepData?.next}`);
   }, [stepData?.next, router, params]);
   return (
-    <nav className="pt-[38px] px-[20px] flex w-full justify-between absolute">
-      <CircleButton type="<" onClick={handleBackClicked} />
+    <nav
+      className={`pt-[38px] px-[20px] flex w-full absolute ${hidePrevButton ? 'justify-end' : 'justify-between'}`}>
+      {!hidePrevButton && <CircleButton type="<" onClick={handleBackClicked} />}
       <CircleButton type=">" onClick={handleNextClicked} disabled={isNextDisabled} />
     </nav>
   );
--- src/components/common/CircleButton.tsx
+++ src/components/common/CircleButton.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { memo } from 'react';
 
 import Button from './Button';
@@ -23,12 +22,11 @@ const CircleButton = ({ type, disabled, onClick }: Props) => {
       className="relative grid h-[34px] w-[34px] place-items-center rounded-full border border-black bg-white drop-shadow-black_light hover:bg-gray-200 disabled:opacity-40"
       disabled={disabled}
       onClick={onClick}>
-      <Image
+      <img
         src={`/icons/${icon[type]}`}
         alt={type}
         width={24}
         height={24}
-        priority
         className={type === '>' ? 'rotate-180' : ''}
       />
     </Button>
